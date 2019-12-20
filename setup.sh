9c54779653e13237b2f9446ccc889cbbd9cdb228
Koch <e100006@c02z60jjlvdt.corp.mastercard.org>
2019-12-20 09:59:12 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/alex-w-k/quick-start/master/.vimrc
 curl https://raw.githubusercontent.com/alex-w-k/quick-start/master/.bashrc >> ~/.bashrc
+mkdir -p ~/.vim/undo
